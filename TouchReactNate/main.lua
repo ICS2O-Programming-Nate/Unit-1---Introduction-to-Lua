a57b557b30d7da6ec4809278adb9057bc99e8a43
Nate MacAvoy <nathaniel.macavoy@ocsbstudent.ca>
2020-03-06 14:29:45 -0500

    3

--- TouchReactNate/main.lua
+++ TouchReactNate/main.lua
@@ -5,3 +5,51 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+--hide status bar 
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set background color 
+display.setDefault("background", 170/255, 40/255, 176/255 )
+
+-- 
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png", 198, 96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible = true 
+
+--
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198, 96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false 
+
+--
+local textObject = display.newText ("Clicked", 0, 0, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = display.contentHeight/3
+textObject:setTextColor (1, 1, 0)
+textObject.isVisible = false
+
+--
+--
+--
+--
+--
+local function blueButtonListener(touch)
+	if (touch.phase == "began") then 
+		blueButton.isVisible = false
+		redButton.isVisible = true 
+		textObject.isVisible = true
+	end
+
+	if (touch.phase == "ended") then 
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false 
+	end
+end
+
+
+--
+blueButton:addEventListener("touch", blueButtonListener)
